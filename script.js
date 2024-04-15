7b0e4ca2f7f060f77b21b7f64b4c40bb482e6014
manassessilva <manasses.2830@gmail.com>
2024-04-15 17:57:12 -0300

    updating the script

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+function colocarDadosNaTela(dados) {
+
+  console.log(dados)
+  document.querySelector(".card_content_legend_title").innerHTML = "Tempo em " + dados.name
+  document.querySelector(".card_content_legend_temperature").innerHTML = Math.floor(dados.main.temp) + "°C"
+  document.querySelector(".card_content_legend_weather").innerHTML = dados.weather[0].description
+  document.querySelector(".card_content_legend_description").innerHTML = "Umidade " + dados.main.humidity + "%"
+
+}
+
 //buscar as informações da cidade no servidor
 //Com o "async" o navegador executa a função de forma asincrona enquanto carrega as demais informações do site.
 async function buscarInformacaoDaCidade(cidade) {
 
   //realizará a consulta ao servidor e retornará as informações da cidade informada pelo usuário
-  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then(Response => Response.json())
+  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&units=metric&lang=pt_br`).then(response => response.json())
 
-  console.log(dados)
-  
+  colocarDadosNaTela(dados)
 }
 
 //Armazenar o nome da cidade ao clicar no botão de pesquisar
